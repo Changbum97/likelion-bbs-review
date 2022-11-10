c5610166ce48f1c52167c08ad7df0733b08ba5aa
Changbum97 <chb2005@naver.com>
2022-11-10 11:07:01 +0900

    프로젝트 생성

---
+++ src/test/java/likelion/springbootbbspreview/SpringbootBbsPreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package likelion.springbootbbspreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBbsPreviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
