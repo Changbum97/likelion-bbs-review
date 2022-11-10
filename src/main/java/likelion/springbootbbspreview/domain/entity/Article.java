8aa420fe74cfce68ee8cb668047637c2faa3b681
Changbum97 <chb2005@naver.com>
2022-11-10 11:32:22 +0900

    Article Entity 생성

--- src/main/java/likelion/springbootbbspreview/domain/entity/Article.java
+++ src/main/java/likelion/springbootbbspreview/domain/entity/Article.java
@@ -1,2 +1,23 @@
-package likelion.springbootbbspreview.domain.entity;public class Article {
+package likelion.springbootbbspreview.domain.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Article2")
+@Getter
+@NoArgsConstructor
+public class Article {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 }
