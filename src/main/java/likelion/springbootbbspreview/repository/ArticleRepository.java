512c2531379f62363901755e68874eadff4c8644
Changbum97 <chb2005@naver.com>
2022-11-10 11:16:03 +0900

    ArticleRepository 생성

--- src/main/java/likelion/springbootbbspreview/repository/ArticleRepository.java
+++ src/main/java/likelion/springbootbbspreview/repository/ArticleRepository.java
@@ -1,2 +1,7 @@
-package likelion.springbootbbspreview.repository;public interface ArticleRepository {
+package likelion.springbootbbspreview.repository;
+
+import likelion.springbootbbspreview.domain.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
