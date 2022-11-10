b1b8ed1d16e66d324228f7ddfb1af1b81d3a505c
Changbum97 <chb2005@naver.com>
2022-11-10 11:42:36 +0900

    findById 기능 구현

--- src/main/java/likelion/springbootbbspreview/controller/ArticleController.java
+++ src/main/java/likelion/springbootbbspreview/controller/ArticleController.java
@@ -5,9 +5,13 @@ import likelion.springbootbbspreview.domain.entity.Article;
 import likelion.springbootbbspreview.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/articles")
@@ -27,10 +31,20 @@ public class ArticleController {
 
     @PostMapping("")
     public String createArticle(CreateArticleDto dto) {
-        System.out.println(dto.getTitle());
         Article newArticle = dto.toEntity();
         articleRepository.save(newArticle);
         log.info("save : {}", newArticle);
         return "redirect:/articles/new";
+    }
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id, Model model) {
+        Optional<Article> optionalArticle = articleRepository.findById(id);
+        if(optionalArticle.isEmpty()) {
+            return "error";
+        }
+
+        model.addAttribute("article", optionalArticle.get());
+        return "articles/show";
     }
 }
